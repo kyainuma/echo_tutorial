9cef9c2fd9df3fdbf1d76c04bd1504ebbc5904e0
kyainuma <yasu6491@icloud.com>
2023-03-08 08:22:18 +0900

    Middleware

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type User struct {
@@ -16,6 +17,29 @@ type User struct {
 func main() {
 	e := echo.New()
 	initRouting(e)
+	// Root level middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Group level middleware
+	g := e.Group("/admin")
+	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "joe" && password == "secret" {
+			return true, nil
+		}
+		return false, nil
+	}))
+
+	// Route level middleware
+	track := func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			println("request to /users")
+			return next(c)
+		}
+	}
+	e.GET("/users", func(c echo.Context) error {
+		return c.String(http.StatusOK, "/users")
+	}, track)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
