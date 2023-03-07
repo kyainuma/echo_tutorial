ac0ce6041698d96408d30952333426cc22e32915
kyainuma <yasu6491@icloud.com>
2023-03-07 09:00:26 +0900

    フォーム

--- server.go
+++ server.go
@@ -16,6 +16,7 @@ func initRouting(e *echo.Echo) {
 	e.GET("/", hello)
 	e.GET("/users/:id", getUser)
 	e.GET("/show", show)
+	e.POST("/save", save)
 }
 
 func hello(c echo.Context) error {
@@ -31,4 +32,10 @@ func show(c echo.Context) error {
 	team := c.QueryParam("team")
 	member := c.QueryParam("member")
 	return c.String(http.StatusOK, "team:" + team + ", member:" + member)
+}
+
+func save(c echo.Context) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name:" + name + ", email:" + email)
 }
