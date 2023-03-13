8f3c15901e9c63ac57831cc7f8fd0c0bd517f371
kyainuma <yasu6491@icloud.com>
2023-03-13 22:34:25 +0900

    Error Handling

--- server.go
+++ server.go
@@ -51,8 +51,21 @@ func (c *CustomContext) Bar() {
 	println("bar")
 }
 
+func customHTTPErrorHandler(err error, c echo.Context) {
+	code := http.StatusInternalServerError
+	if he, ok := err.(*echo.HTTPError); ok {
+		code = he.Code
+	}
+	c.Logger().Error(err)
+	errorPage := fmt.Sprintf("%d.html", code)
+	if err := c.File(errorPage); err != nil {
+		c.Logger().Error(err)
+	}
+}
+
 func main() {
 	e := echo.New()
+	e.HTTPErrorHandler = customHTTPErrorHandler
 	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			cc := &CustomContext{c}
